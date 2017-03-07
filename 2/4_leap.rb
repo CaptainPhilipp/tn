3454597845044b152f83981062dac2e6d1f86347
CaptainPhilipp <Ph-s@mail.ru>
2017-03-08 01:33:41 +0300

    restrut with proc in 2.4

--- 2/4_leap.rb
+++ 2/4_leap.rb
@@ -5,14 +5,10 @@
 # вроде Date#yday или Date#leap?)
 # Алгоритм опредления високосного года: www.adm.yar.ru
 
+DAYCOUNTS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 0 - нулябрь. вроде так иногда делают?
+
 def leap?(year)
   year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-end
-
-def days_in_month(month, year)
-  counts = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 0 что бы убрать лишний if
-
-  counts[month] + ( month == 2 && leap?(year) ? 1 : 0 )
 end
 
 puts "Введите число, месяц, год"
@@ -20,12 +16,15 @@ answer = gets.chomp.split(' ').map(&:to_i)
 raise "не-а" unless answer.size == 3
 day, month, year = answer
 raise "wrong month" unless (1..12).include? month
-raise "wrong day"   unless (1..days_in_month(month, year)).include? day
 
-dayscount = day
+dayscount = ->(monh = month) { DAYCOUNTS[monh] + ( monh == 2 && leap?(year) ? 1 : 0 ) }
+
+raise "wrong day" unless (1..dayscount.call).include? day
+
+count = day
 (1..month).each do |month|
   previous_month = month - 1
-  dayscount += days_in_month(previous_month, year)
+  count += dayscount.call(previous_month)
 end
 
-puts dayscount
+puts count
