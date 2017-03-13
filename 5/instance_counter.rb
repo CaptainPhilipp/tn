a67c997422c8df0f015da1f21a5fb812fde55bda
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-03-13 12:59:26 +0300

    InstanceCounter total count to every class count
    # забавное наблюдение: метод `<<` для инст класс перемеменной есть, а `=` - нет; пришлось объявлять еще один метод.

--- 5/instance_counter.rb
+++ 5/instance_counter.rb
@@ -1,27 +1,28 @@
 module InstanceCounter
-  # Создать модуль InstanceCounter, содержащий следующие методы класса и инстанс-методы, которые подключаются автоматически при вызове include в классе:
-  # ::instances ол-во экземпляров данного класса
-  # #register_instance увеличивает счетчик кол-ва экземпляров класса
-  #   подключаются автоматически при вызове include в классе
-  #   можно вызвать из конструктора
-  #   не публичный
-
-  # сработает только с классами, где initialize не определен,
-  # потому дя очевидности уберу отсюда
-  # def initialize
-  #   register_instance
-  #   super
-  # end
-
-  @@instances = 0
-
-  def self.instances
-    @@instances
+  def self.included(klass)
+    klass.extend ClassMethods
+    klass.include InstanceMethods
+  end
+  
+  module ClassMethods
+    @instances = 0
+    
+    def instances
+      @instances
+    end
+    
+    protected
+    
+    def register_instance
+      @instances += 1
+    end
   end
 
-  protected
-
-  def register_instance
-    @@instances += 1
-  end
+  module InstanceMethods
+    protected
+    
+    def register_instance
+      self.class.register_instance
+    end
+  end # InstanceMethods
 end
