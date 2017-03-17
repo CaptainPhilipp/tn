272efb4d61ecaeec00d4c9383a5414a00385eec3
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-03-17 15:16:08 +0300

    L9 Accessors

--- 9/accessors.rb
+++ 9/accessors.rb
@@ -1,20 +1,53 @@
 module Accessors
-  # геттеры и сеттеры для любого кол-ва атрибутов,
-  # при этом сеттер сохраняет все значения инстанс-переменной
-  # при изменении этого значения.
-  def attr_accessor_with_history(*args)
-    # class
+  attr_accessor :access_history
+
+  # getter, setter
+  # history of setter
+  def attr_accessor_with_history(*names)
+    @access_history = {}
+    names.each do |name|
+      create_getter(name)
+      create_history(name)
+      create_history_setter(name)
+    end
   end
 
-  # В класс, в который подключается модуль должен добавляться инстанс-метод
-  # <имя_атрибута>_history
+  # input: foo: String, bar: Integer, baz: Array
+  def strong_attr_accessor(hash)
+    hash.each do |name, klass|
+      create_getter(name)
+      klass = klass.is_a?(Class) ? klass : const_get(klass.to_sym)
+      create_setter(name) do |value|
+        raise ArgumentError, 'Wrong argument class' unless value.is_a?(klass)
+      end
+    end
+  end
 
-  # принимает имя атрибута и его класс
-  # создается геттер и сеттер для одноименной инстанс-переменной
-  #  но сеттер проверяет тип присваемоего значения
-  # Если тип отличается от того, который указан вторым параметром,
-  # то выбрасывается исключение
-  def strong_attr_acessor(name, klass)
-    # class
+  protected
+
+  def create_getter(name)
+    define_method(name.to_sym) { instance_variable_get("@#{name}".to_sym) }
+  end
+
+  # может служить оберткой для расширения сеттеров
+  def create_setter(name)
+    varname  = "@#{name}".to_sym
+    methname = "#{name}=".to_sym
+    define_method(methname) do |value|
+      yield(value) if block_given?
+      instance_variable_set(varname, value)
+    end
+  end
+
+  # расширенный setter
+  def create_history_setter(name)
+    create_setter(name) { |value| self.access_history[name] << value }
+  end
+
+  def create_history(name)
+    varname  = "@#{name}_history".to_sym
+    methname = "#{name}_history".to_sym
+    self.access_history[name] = []
+    define_method(methname) { self.class.access_history[name] }
   end
 end
