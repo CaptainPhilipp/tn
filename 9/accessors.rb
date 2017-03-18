967ceb8f8f284845dad589d1a975f316544f644a
CaptainPhilipp <Ph-s@mail.ru>
2017-03-18 14:24:00 +0300

    L9 After review fix

--- 9/accessors.rb
+++ 9/accessors.rb
@@ -3,55 +3,40 @@ module Accessors
   # history of setter
   def attr_accessor_with_history(*names)
     names.each do |name|
-      create_getter(name)
-      create_history_getter(name)
-      create_history_setter(name)
-    end
+      # create_getter
+      varname = "@#{name}".to_sym
+      define_method(name.to_sym) { instance_variable_get(varname) }
+
+      # create_history_getter
+      hist_var  = "@#{name}_history".to_sym
+      define_method("#{name}_history".to_sym) { instance_variable_get(hist_var) }
+
+      # create_history_setter
+      define_method("#{name}=".to_sym) do |value|
+        if instance_variable_defined?(hist_var)
+          history = instance_variable_get(hist_var)
+          instance_variable_set(hist_var, history << value)
+        else
+          instance_variable_set(hist_var, [value])
+        end
+        instance_variable_set(varname, value)
+      end
+    end # names.each do
   end
 
   # input: foo: String, bar: Integer, baz: Array
   def strong_attr_accessor(hash)
     hash.each do |name, klass|
-      create_getter(name)
-      create_setter(name) do |value|
+      # create_getter
+      define_method(name.to_sym) { instance_variable_get("@#{name}".to_sym) }
+
+      # create_setter
+      var  = "@#{name}".to_sym
+      meth = "#{name}=".to_sym
+      define_method(meth) do |value|
         raise ArgumentError, 'Wrong argument class' unless value.is_a?(klass)
+        instance_variable_set(var, value)
       end
     end
-  end
-
-  protected
-
-  def create_getter(name)
-    define_method(name.to_sym) { instance_variable_get("@#{name}".to_sym) }
-  end
-
-  def create_setter(name)
-    var  = "@#{name}".to_sym
-    meth = "#{name}=".to_sym
-    define_method(meth) do |value|
-      yield if block_given?
-      instance_variable_set(var, value)
-    end
-  end
-
-  def create_history_setter(name)
-    hist_var = "@#{name}_history".to_sym
-
-    define_method("#{name}=".to_sym) do |value|
-      if instance_variable_defined?(hist_var)
-        history = instance_variable_get(hist_var)
-        instance_variable_set(hist_var, history << value)
-      else
-        instance_variable_set(hist_var, [value])
-      end
-
-      instance_variable_set("@#{name}".to_sym, value)
-    end
-  end
-
-  def create_history_getter(name)
-    hist_varname  = "@#{name}_history".to_sym
-    hist_methname = "#{name}_history".to_sym
-    define_method(hist_methname) { instance_variable_get(hist_varname) }
   end
 end
--- 9/validation.rb
+++ 9/validation.rb
@@ -6,11 +6,11 @@ class InvalidVolume   < InvalidData; end
 
 module Validation
   def self.included(klass)
-    klass.include Includeable
-    klass.extend  Extendeable
+    klass.extend  ClassMethods
+    klass.include InstanceMethods
   end
 
-  module Includeable
+  module InstanceMethods
     def validate!
       tasks = self.class.class_variable_get(:@@validating_tasks)
       tasks.each { |name, task| duck_type_send(name, task) }
@@ -26,8 +26,8 @@ module Validation
 
     def duck_type_send(name, task)
       current_value = instance_variable_get("@#{name}".to_sym)
-      meth = duck_type(task.requirement)
-      self.class.send(meth, name, current_value, task)
+      meth = duck_type(task[:requirement])
+      send(meth, name, current_value, task)
     end
 
     def duck_type(requirement)
@@ -42,24 +42,43 @@ module Validation
         end
       "validate_#{type}".to_sym
     end
-  end # Includeable
 
-  module Extendeable
+    def validate_presense(name, value, _)
+      return unless value.nil? || (value.respond_to?(:empty?) && value.empty?)
+      raise InvalidPresense, "#{name} is nil or empty (#{value})"
+    end
+
+    def validate_format(name, current_value, task)
+      return if current_value =~ task[:requirement]
+      raise InvalidFormat, "#{name} has wrong format"
+    end
+
+    def validate_type(name, current_value, task)
+      return if current_value.is_a? task[:requirement]
+      raise InvalidType, "#{name} is a #{current_value.class}, not #{task[:type}"
+    end
+
+    def validate_range(name, current_value, task)
+      return if task[:requirement].cover? current_value
+      raise InvalidVolume, "#{name} (#{current_value}) " \
+                           "not in #{task[:requirement]}"
+    end
+
+    def validate_integer(name, current_value, task)
+      return if task[:requirement].send(task[:argument], current_value)
+      raise InvalidVolume, "#{name}(#{current_value}): #{task[:requirement]} " \
+                           "must be #{task[:argument]} #{current_value}"
+    end
+  end # InstanceMethods
+
+  module ClassMethods
     # validate :var           # not nil or not empty
-    # validate :var, /regexp/ # #=~()
+    # validate :var, /regexp/ # #=~
     # validate :var, Class    # #is_a?
     # validate :var, 0..12    # #cover?
     # validate :var, 10, :<   # "10 sould_be less(<) then @var"
     def validate(name, requirement = nil, argument = nil)
-      set_task(name, requirement, argument)
-    end
-
-    private
-
-    ValidateTask = Struct.new :requirement, :argument
-
-    def set_task(name, requirement, argument)
-      task = ValidateTask.new(requirement, argument)
+      task = [requirement: requirement, argument: argument]
 
       if class_variable_defined?(:@@validating_tasks)
         tasks = class_variable_get(:@@validating_tasks)
@@ -70,32 +89,5 @@ module Validation
         class_variable_set(:@@validating_tasks, name => task)
       end
     end
-
-    def validate_presense(name, value, _)
-      return unless value.nil? || (value.respond_to?(:empty?) && value.empty?)
-      raise InvalidPresense, "#{name} is nil or empty (#{value})"
-    end
-
-    def validate_format(name, current_value, task)
-      return if current_value =~ task.requirement
-      raise InvalidFormat, "#{name} has wrong format"
-    end
-
-    def validate_type(name, current_value, task)
-      return if current_value.is_a? task.requirement
-      raise InvalidType, "#{name} is a #{current_value.class}, not #{task.type}"
-    end
-
-    def validate_range(name, current_value, task)
-      return if task.requirement.cover? current_value
-      raise InvalidVolume, "#{name} (#{current_value}) " \
-                           "not in #{task.requirement}"
-    end
-
-    def validate_integer(name, current_value, task)
-      return if task.requirement.send(task.argument, current_value)
-      raise InvalidVolume, "#{name}(#{current_value}): #{task.requirement} " \
-                           "must be #{task.argument} #{current_value}"
-    end
-  end # Extendeable
+  end # ClassMethods
 end
